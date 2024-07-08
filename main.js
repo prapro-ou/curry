519dcd69cd42afac060de6a4720953dddff56587
inuijura <166824531+inuijura@users.noreply.github.com>
2024-07-08 14:48:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -24,18 +24,57 @@ con.msimageSmoothingEnabled     = false;
 let frameCount = 0;
 let startTime;
 
+let chImg1 = new Image();
+chImg1.src = "yashinoki1.png";
+chImg1.onload = draw;
+
+let chImg2 = new Image();
+chImg2.src = "yashinoki2.png";
+chImg2.onload = draw;
+
+let chImg3 = new Image();
+chImg3.src = "yashinoki3.png";
+chImg3.onload = draw;
+
+let chImg4 = new Image();
+chImg4.src = "yashinoki4.png";
+chImg4.onload = draw;
+
+let chImg5 = new Image();
+chImg5.src = "same1.png";
+chImg5.onload = draw;
+
+let chImg6 = new Image();
+chImg6.src = "same2.png";
+chImg6.onload = draw;
+
+let chImg7 = new Image();
+chImg7.src = "tetorapodo.png";
+chImg7.onload = draw;
+
+let chImg8 = new Image();
+chImg8.src = "kobune.png";
+chImg8.onload = draw;
+
+let chImg9 = new Image();
+chImg9.src = "iruka.png";
+chImg9.onload = draw;
+
+
 //更新処理
 function update()
 {
 
 }
 
+
 //描画処理
 function draw()
 {
     //画面を水色でクリア
-    vcon.fillStyle = "#000055";
+    vcon.fillStyle = "#66AAFF";
     vcon.fillRect(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H);
+
 
     //デバッグ情報を表示
     vcon.font = "24px 'Impact'"; 
@@ -43,6 +82,28 @@ function draw()
     vcon.fillText("FRAME:"+frameCount, 10, 20);
 
     con.drawImage(vcan, 0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, 0, 0, SCREEN_SIZE_W*3, SCREEN_SIZE_H*3);
+
+    if((frameCount%100) <= 25){
+        con.drawImage(chImg2, 0, 0, 16, 16, 126, 126, 64, 64);
+    }
+    else if((frameCount%100) > 25 && (frameCount%100) <= 50){ 
+        con.drawImage(chImg3, 0, 0, 16, 16, 126, 126, 64, 64);
+    }
+    else if((frameCount%100) > 50 && (frameCount%100) <= 75){ 
+        con.drawImage(chImg1, 0, 0, 16, 16, 126, 126, 64, 64);
+    }
+    else con.drawImage(chImg4, 0, 0, 16, 16, 126, 126, 64, 64);
+
+
+    if((frameCount%50) <= 25){
+        con.drawImage(chImg5, 0, 0, 16, 16, 200, 400, 64, 64);
+    }
+    else con.drawImage(chImg6, 0, 0, 16, 16, 200, 400, 64, 64);
+
+    con.drawImage(chImg7, 0, 0, 16, 16, 50, 350, 64, 64);
+    con.drawImage(chImg8, 0, 0, 16, 16, 100, 450, 64, 64);
+    con.drawImage(chImg9, 0, 0, 16, 16, 300, 500, 64, 64);
+
 
 }
 
