93848831d534c226b08d98258fb70ba6cc57bae7
inuijura <166824531+inuijura@users.noreply.github.com>
2024-07-18 14:25:46 +0900

    Add files via upload

---
+++ normal_stage3/const_normal_stage.js
@@ -0,0 +1,15 @@
+//
+// 定数の定義用JS
+//
+
+const GAME_FPS = 1000/60;
+const SCREEN_SIZE_W = 224;     //画面の横サイズ
+const SCREEN_SIZE_H = 256;     //画面の縦サイズ
+
+//一画面当たりのブロックの数
+const MAP_SIZE_W    = SCREEN_SIZE_W / 16;
+const MAP_SIZE_H    = SCREEN_SIZE_H / 16;
+
+//マップデータのブロックの数
+const FILED_SIZE_W = 16;
+const FILED_SIZE_H = 256;
